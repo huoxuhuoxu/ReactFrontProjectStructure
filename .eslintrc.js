732296f3c4cedd5a90696e32cb05af5cd03ef54b
huzhen <zhen.hu@evenhidata.com>
2017-10-13 01:07:24 +0800

    升级语法检测

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,44 @@
-
 module.exports = {
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6":true
-    },
-    "ecmaFeatures": {},
-    "rules": {
-        "no-console": "warn",
-        "eqeqeq": "error",
-        "no-alert": "warn",
-        "no-eval": "error",
-        "semi": [
-            "error",
-            "always"
-        ],
-        "strict": "error",
-        "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }]
+  "parser": "babel-eslint",
+
+  "plugins": ["react"],
+
+  "extends": [
+    "eslint:recommended", "plugin:react/recommended"
+  ],
+
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
     }
+  },
+
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "mocha": true
+  },
+
+  "ecmaFeatures": {},
+  
+  "rules": {
+    "no-console": "warn",
+    "eqeqeq": "error",
+    "no-alert": "warn",
+    "no-eval": "error",
+    "semi": [
+      "error", "always"
+    ],
+    "strict": "error",
+    "no-unused-vars": [
+      "error", {
+        "vars": "all",
+        "args": "none",
+        "ignoreRestSiblings": false
+      }
+    ]
+  }
 };
